3a7f064993f185a89832fffc1e1c6870944a9ed0
Naima Malik <noma_m@hotmail.com>
2023-10-09 22:03:50 +0200

    Completed exercises for week 2

--- weeks/week-2/exercises/exercise-3/index.js
+++ weeks/week-2/exercises/exercise-3/index.js
@@ -1,6 +1,5 @@
 // Loops & Objects Exercise
 
-
 const pokemons = [
   "Bulbasaur",
   "Ivysaur",
@@ -22,24 +21,56 @@ const pokemons = [
 // the pokemons from the pokemons array
 // invoke the function to see that it works
 
+for (let i = 0; i < pokemons.length; i++) {
+  console.log(pokemons[i]);
+}
+
+// Creating function
+const listPokenames = (pokename) => {
+  console.log(pokename);
+  // };
+  // Add function to forEach()
+  pokemons.forEach(listPokenames);
+
+  // Common way is through annonymous functions:
+  pokemons.forEach((pokename) => {
+    console.log(pokename);
+  });
+};
 // 2)
 // Create an object for the pokemon Pikachu.
 // it could have properties such as name, element,
 // strength, color, amountOfLegs or something like that.
+
+const pikachu = {
+  name: "pikachu",
+  element: "electric-type",
+  strength: 32,
+  color: "yellow",
+  amountOfLegs: 2,
+};
 
 // 3)
 // console.log something like
 // 'Pikachu is a electricity pokemon with the strength of 32'
 // Selecting some of the information about your pikachu object.
 
+console.log(
+  `Pikachu is an ${pikachu.element} pokemon with the strengh of ${pikachu.strength}`
+);
+
 // 4)
 // Add the property stillToCatch: true to the Pikachu object.
+pikachu.stillToCatch = true;
+console.log(pikachu);
 
 // 5)
 // change the value of the key strength in the Pikachu object.
+pikachu.strength = 40;
 
 // 6)
 // Delete a property from the Pikachu object.
+delete pikachu.stillToCatch;
 
 // 7)
 // Fill up this array with a few more pokemons.
@@ -57,12 +88,41 @@ const moreAboutPokemons = [
     element: "fire",
     strength: 32,
   },
+  {
+    name: "Misty",
+    element: "air",
+    strength: 20,
+  },
+  {
+    name: "Cacoon",
+    element: "earth",
+    strength: 40,
+  },
 ];
 
 // 8)
 // Print out all the NAMES of the pokemons in this new array.
+for (let i = 0; i <= moreAboutPokemons.length; i++) {
+  console.log(moreAboutPokemons[i]);
+}
+
+const pokemonDetails = (name) => {
+  console.log(name);
+};
+
+moreAboutPokemons.forEach(pokemonDetails);
+
+moreAboutPokemons.forEach((name) => {
+  console.log(name);
+});
 
 // 9)
 // Print out something like
 // 'Bulbasaur is a water pokemon with the strength of 32'
 // Do this for all the pokemons.
+
+moreAboutPokemons.forEach((pokemon) => {
+  console.log(
+    `${pokemon.name} is a ${pokemon.element} pokémon with the strength of ${pokemon.strength}`
+  );
+});
--- weeks/week-2/exercises/exercise-4/index.js
+++ weeks/week-2/exercises/exercise-4/index.js
@@ -13,54 +13,166 @@ const pokemons = [
   "Metapod",
   "Weedle",
   "Pikachu",
-  "Pidgey"
+  "Pidgey",
 ];
 
 const countThem = () => {
   // print out, in a sentence, how many pokemons I have.
   // like: "I have x pokemons!"
+  console.log(`I have ${pokemons.length} pokemons`);
 };
+countThem();
 
 const orderThem = () => {
   // order the pokemons alphabetically
+  const orderedPokemons = pokemons.sort((a, b) => a[1].localeCompare(b[1]));
+  console.log("This is the ordered array:", orderedPokemons);
 };
+
+orderThem();
 
 const flipThem = () => {
   // reverse the order of the pokemons
+  const reverse = pokemons.reverse();
+  console.log(reverse);
 };
+
+flipThem();
 
 const makeThemBig = () => {
   // print the pokemons in UPPERCASE letters
+
+  //Using for classic loop
+  for (let i = 0; i < pokemons.length; i++) {
+    console.log(pokemons[i].toUpperCase());
+  }
+
+  // Using forEach method
+  pokemons.forEach((pokemon) => {
+    console.log(pokemon.toUpperCase());
+  });
 };
+
+makeThemBig();
 
 const onlyTheBs = () => {
   // only print the pokemons that starts with B
+
+  // Using for loop
+  for (let i = 0; i < pokemons.length; i++) {
+    if (pokemons[i].includes("B")) {
+      console.log("This is an array with only Bs:", pokemons);
+    }
+  }
+  // Using forEach method
+  pokemons.forEach((name) => {
+    if (name.includes("B")) {
+      console.log(name);
+    }
+  });
 };
+
+onlyTheBs();
 
 const notTheCs = () => {
   // remove all pokemons that starts with C
+
+  for (let i = 0; i < pokemons.length; i++) {
+    if (!pokemons[i].includes("C")) {
+      console.log("These are the filtered pokemons:", pokemons);
+    }
+  }
+
+  const filtered = pokemons.filter((pokemon) => {
+    if (!pokemon.includes("C")) {
+      return true;
+    }
+  });
+  console.log(filtered);
 };
 
 const nameAndIdThanks = () => {
   // print out name and index of all pokemons
   // like: number x - Squirtle
-};
 
-const catchPokemon = name => {
+  //Loop with for loop
+  for (let i = 0; i < pokemons.length; i++) {
+    console.log(`Number ${i + 1} - ${pokemons[i]}`);
+  }
+  // Loop with forEach method
+  pokemons.forEach((pokemon, index) =>
+    console.log(`Number ${index + 1} - ${pokemon}`)
+  );
+};
+nameAndIdThanks();
+
+const catchPokemon = (name) => {
   // add a pokemon with a name of your choice to the list,
   // print the list so you see its there.
+  pokemons.push(name);
+  console.log(pokemons);
 };
 
-const didICatchIt = name => {
+catchPokemon("Misty-thunder");
+
+const didICatchIt = (name) => {
   // check the pokemons to see if a specific pokemon is in the array
+
+  //Using for loop
+  for (let i = 0; i < pokemons.length; i++) {
+    if (pokemons[i].includes(name)) {
+      console.log(`Ye! ${name} does exist!`);
+    } else {
+      console.log(`No! ${name} does not exist!!`);
+    }
+  }
+
+  //Another way
+  if (pokemons.includes(name)) {
+    console.log(`Yes! ${name} does exist!`);
+  } else {
+    console.log(`No! ${name} does not exist!!`);
+  }
 };
+
+didICatchIt("Wartortle");
 
 const addInThirdPlace = () => {
   // add the pokemon "Clefairy" in the third place of the array
-  // print the list so you see its there.
+  pokemons.splice(2, 0, "Clefairy");
+  console.log("Third array added:", pokemons);
 };
+addInThirdPlace();
 
 // ***BONUS***
 const theLongestName = () => {
   // find the pokemon with the longest name
+  // for (let i = 0; i < pokemons.length; i++) {
+  //   for(let j = 0; i <)
+  //   console.log(pokemons[i].length);
+  // }
+
+  const longestName = pokemons.reduce(function (a, b) {
+    return a.length > b.length ? a : b;
+  });
+  console.log(longestName);
+
+  // let lgth = 0;
+  // let longest;
+  // for (let i = 0; i < pokemons.length; i++) {
+  //   if (pokemons[i].length > lgth) {
+  //     lgth = pokemons[i].length;
+  //     longest = pokemons[i];
+  //   }
+  // }
+  // console.log(longest);
+
+  function getLongestString() {
+    return pokemons.sort((a, b) => {
+      return b.length - a.length;
+    })[0];
+  }
+  console.log(getLongestString());
 };
+
+theLongestName();
